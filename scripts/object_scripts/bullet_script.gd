9d3282ce707389a2ffed144e39a5916a116be0d1
Gingelduff101 <bram.bakker21@gmail.com>
2025-07-06 16:28:02 +0200

    actually saved files this time

--- scripts/object_scripts/bullet_script.gd
+++ scripts/object_scripts/bullet_script.gd
@@ -1,1 +1,18 @@
-extends Node
+extends Area2D
+class_name bullet
+
+@export var bullet_speed: float = 500.0
+
+var dir: Vector2
+
+func _ready() -> void:
+	
+	var mouse_pos = get_global_mouse_position()
+	dir = (mouse_pos - self.global_position).normalized()
+
+func _physics_process(delta: float) -> void:
+	position += dir * bullet_speed * delta
+	
+	if self.has_overlapping_bodies():
+		print(self.get_overlapping_bodies())
+		self.queue_free()
--- scripts/player_scripts/player_attack.gd
+++ scripts/player_scripts/player_attack.gd
@@ -4,11 +4,24 @@ extends Node
 
 @export var player_hitbox_size: float = 70.0
 
-func _input(event: InputEvent) -> void:
-	#Checks if the body is selected and if so, lets the characterbody2d look at the mouse
+var bullet_spawn_point_multiplier: float = 1.2
+
+@export var rate_of_fire: float = 1.0
+@export var attack_cooldown_timer: Timer
+
+func _ready() -> void:
+	attack_cooldown_timer.wait_time = rate_of_fire
+
+func _process(delta: float) -> void:
+	
+	if !attack_cooldown_timer.is_stopped():
+		return
 	
 	if Input.is_action_just_pressed("LMB"):
+		
 		if body:
+			if attack_cooldown_timer.is_stopped():
+				attack_cooldown_timer.start()
 			var bullet = preload("res://scenes/object_scenes/bullet.tscn").instantiate()
 			
 			var mouse_pos := body.get_global_mouse_position()
@@ -16,7 +29,10 @@ func _input(event: InputEvent) -> void:
 			
 			var bullet_dir: Vector2 = (mouse_pos - body.global_position).normalized()
 			
-			bullet.global_position = body.global_position
+			var bullet_spawn_distance : float = player_hitbox_size * bullet_spawn_point_multiplier
+			bullet.global_position = body.global_position + bullet_dir * bullet_spawn_distance
+			
+			
 			bullet.rotation = shoot_angle
 			
 			get_tree().root.add_child(bullet)
