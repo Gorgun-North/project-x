4a8e1f4f1c39bedfee35a8af47c05e85b8287a31
Gingelduff101 <bram.bakker21@gmail.com>
2025-07-10 00:29:41 +0200

    fsaasflksa

--- scripts/support_scripts/game_controller.gd
+++ scripts/support_scripts/game_controller.gd
@@ -12,10 +12,15 @@ func _ready() -> void:
 		navmesh.bake_navigation_polygon()
 		
 func _on_bake_navmesh():
+	if !get_tree():
+		return
+	
 	if navmesh:
+		
 		var navpol: NavigationPolygon = navmesh.navigation_polygon
 		
 		while navmesh.is_baking() == true:
-			await get_tree().process_frame
+			if get_tree():
+				await get_tree().process_frame
 		
 		navmesh.bake_navigation_polygon()
